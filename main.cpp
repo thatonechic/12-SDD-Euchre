6743418ad9ffe60455c910ee84cf1465b3582adb
wilsam239 <swilliamson239@hotmail.com>
2015-03-05 22:15:07 +1100

    Create main.cpp
    main program file.

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+	||||||||||||||||||||||||
+	||                    ||
+	|| ~Euchre Card Game~ ||
+	||                    ||
+	||||||||||||||||||||||||
+	
+	Contributors:
+	
+		Sam Williamson
+		Jackson Valiukas
+		Peter Wood
+		Amelia Paton
+		Ethan Harvey
+		Tome Greenfield
+		Daniel McDougall
+	
+	Date Started: 5th March 2015
+	Version: 1.0
+
+*/
+
+
+
+
+#include <iostream>
+#include <cstdlib>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	
+	return EXIT_SUCCESS;
+}
